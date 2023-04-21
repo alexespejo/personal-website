03f4d13940fa15cb91dfcbd67548146e80bc8c7c
Alex Espejo <aespejo102@gmail.com>
2023-04-20 18:03:19 -0700

    added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- modules: ["nuxt-icon", "@pinia/nuxt"],
+ modules: ["nuxt-icon", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+ postcss: {
+  plugins: {
+   tailwindcss: {},
+   autoprefixer: {},
+  },
+ },
  pinia: {
   autoImports: [
    // automatically imports `defineStore`
