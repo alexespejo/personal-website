ce61c99e0b6662b3935b22f90913e95a8a919839
Alex Espejo <aespejo102@gmail.com>
2023-04-20 17:52:11 -0700

    revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   autoImports: [
    // automatically imports `defineStore`
    "defineStore", // import { defineStore } from 'pinia'
-   ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
   ],
  },
 });
