eb8698fe47f39c43111b80a9067b92dd23dd14c3
Alex Espejo <aespejo102@gmail.com>
2023-04-21 00:01:29 -0700

    update

--- app.vue
+++ app.vue
@@ -1,15 +1,3 @@
-<script setup lang="ts">
-useServerSeoMeta({
- title: "Alexander Espejo's Portfolio",
- ogTitle: "Alexander Espejo's Portfolio",
- description:
-  "My name is Alexander Espejo, I'm a first year computer science major at Orange Coast College 🎓 I'm a self-taught frontend developer 👨‍💻 and aspring software engineer ⚙️ (check out my Github if you get the chance 🤩)profile picture I love to develop software 🖥️ and build apps 📱 that can make a positive impact on the world I'm always eager to learn and practice new skills 🏋️‍♂️ and technologies, and I enjoy collaborating with other passionate coders 🤝. I'm excited to share my projects and ideas with you! 🫶",
- ogDescription:
-  "My name is Alexander Espejo, I'm a first year computer science major at Orange Coast College 🎓 I'm a self-taught frontend developer 👨‍💻 and aspring software engineer.",
- ogImage: "../assets/profilePIC.png",
- twitterCard: "summary_large_image",
-});
-</script>
 <template>
  <NuxtPage />
 </template>
