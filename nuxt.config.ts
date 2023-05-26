b30cc9abfd0247f9cd8e9808e9ff2de07a3a115f
Alex Espejo <aespejo102@gmail.com>
2023-05-25 17:45:04 -0700

    test chatgpt integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- modules: ["nuxt-icon", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+ modules: ["nuxt-icon", "@pinia/nuxt", "@nuxtjs/tailwindcss", "nuxt-chatgpt"],
  postcss: {
   plugins: {
    tailwindcss: {},
    autoprefixer: {},
   },
+ },
+ chatgpt: {
+  apiKey: process.env.NUXT_API_SECRET,
  },
  pinia: {
   autoImports: [
